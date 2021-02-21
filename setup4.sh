b8437a69639976761e3c48b945b26aaf222c9166
Ariel <arielaleksandrus@hotmail.com>
2021-02-20 23:53:59 -0300

    Adds more backups

--- setup4.sh
+++ setup4.sh
@@ -7,7 +7,7 @@ USERNAME="$(whoami)"
 # Create the cron jobs
 (crontab -l 2>/dev/null; echo "@reboot /home/${USERNAME}/NCommerce/ncommerce_api/vendor/scripts/start_ngrok.sh /home/${USERNAME}/NCommerce/ncommerce_api | at now + 2 minutes") | crontab -
 sudo su <<EOF
-(crontab -l 2>/dev/null; echo -e "* * * * * /home/${USERNAME}/NCommerce/ncommerce_api/vendor/scripts/chown_files.sh /home/${USERNAME}/NCommerce/ncommerce_api\n* * * * * ( sleep 30; /home/${USERNAME}/NCommerce/ncommerce_api/vendor/scripts/chown_files.sh /home/${USERNAME}/NCommerce/ncommerce_api )\n30 17 * * 4 /home/${USERNAME}/NCommerce/ncommerce_api/vendor/scripts/backup.sh /home/${USERNAME}/NCommerce/ncommerce_api") | crontab -
+(crontab -l 2>/dev/null; echo -e "* * * * * /home/${USERNAME}/NCommerce/ncommerce_api/vendor/scripts/chown_files.sh /home/${USERNAME}/NCommerce/ncommerce_api\n* * * * * ( sleep 30; /home/${USERNAME}/NCommerce/ncommerce_api/vendor/scripts/chown_files.sh /home/${USERNAME}/NCommerce/ncommerce_api )\n30 18 * * 2 /home/${USERNAME}/NCommerce/ncommerce_api/vendor/scripts/backup.sh /home/${USERNAME}/NCommerce/ncommerce_api\n30 17 * * 4 /home/${USERNAME}/NCommerce/ncommerce_api/vendor/scripts/backup.sh /home/${USERNAME}/NCommerce/ncommerce_api\n30 18 * * 6 /home/${USERNAME}/NCommerce/ncommerce_api/vendor/scripts/backup.sh /home/${USERNAME}/NCommerce/ncommerce_api") | crontab -
 EOF
 
 sudo service cron reload
