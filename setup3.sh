344a71d5f9abe83e7318a201a2982843a4424d43
Ariel Aleksandrus <ArielAleksandrus@users.noreply.github.com>
2023-07-10 19:37:31 -0300

    Stopped using Ngrok. Use tunnel.nlabs.live instead

--- setup3.sh
+++ setup3.sh
@@ -1,12 +1,13 @@
+# DEPRECATED: nao usaremos mais ngrok
 # Agora que as dependências foram instaladas, vamos configurar o NCommerce
-NGROK_FILE=~/.ngrok2/ngrok.yml
+#NGROK_FILE=~/.ngrok2/ngrok.yml
 
-if test -f "$NGROK_FILE" ; then
-  echo "ngrok file present. Proceeding..."
-else
-  echo "FAILED: ngrok file is not set! use 'ngrok authtoken < ngrok token here>'"
-  exit 1
-fi
+#if test -f "$NGROK_FILE" ; then
+#  echo "ngrok file present. Proceeding..."
+#else
+#  echo "FAILED: ngrok file is not set! use 'ngrok authtoken < ngrok token here>'"
+#  exit 1
+#fi
 
 if test -n "${1-}"; then
   echo "ncommerce token present. Proceeding..."
@@ -22,8 +23,13 @@ else
   exit 1
 fi
 
+# DEPRECATED: nao usaremos mais ngrok
 # Configurar ngrok
-cat ~/NCommerce/ncommerce_api/essentials/ngrok-files/example.yml >> ~/.ngrok2/ngrok.yml
+#cat ~/NCommerce/ncommerce_api/essentials/ngrok-files/example.yml >> ~/.ngrok2/ngrok.yml
+
+# Adicionar tunnel.nlabs.live e nlabs.live ao known hosts
+ssh-keyscan -H nlabs.live >> ~/.ssh/known_hosts
+ssh-keyscan -H tunnel.nlabs.live >> ~/.ssh/known_hosts
 
 # Configurar nginx para ncommerce-api (back end)
 curl https://ncommerce.app:3001/setup/nginx_back/$2?token=$1 > /tmp/ncommerce-api
