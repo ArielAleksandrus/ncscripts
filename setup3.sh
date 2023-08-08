cfa107003a4da3dc237e76d0674f9868b0ea753f
Ariel <arielaleksandrus@hotmail.com>
2023-08-08 17:15:06 -0300

    Replaces ngrok with tunnel.nlabs.live

--- setup3.sh
+++ setup3.sh
@@ -1,12 +1,12 @@
 # Agora que as dependências foram instaladas, vamos configurar o NCommerce
-NGROK_FILE=~/.ngrok2/ngrok.yml
+#NGROK_FILE=~/.ngrok2/ngrok.yml
 
-if test -f "$NGROK_FILE" ; then
-  echo "ngrok file present. Proceeding..."
-else
-  echo "FAILED: ngrok file is not set! use 'ngrok authtoken < ngrok token here>'"
-  exit 1
-fi
+#if test -f "$NGROK_FILE" ; then
+#  echo "ngrok file present. Proceeding..."
+#else
+#  echo "FAILED: ngrok file is not set! use 'ngrok authtoken < ngrok token here>'"
+#  exit 1
+#fi
 
 if test -n "${1-}"; then
   echo "ncommerce token present. Proceeding..."
@@ -22,8 +22,13 @@ else
   exit 1
 fi
 
+# DEPRECATED: ngrok usage will be removed, and replaced with tunnel.nlabs.live
 # Configurar ngrok
 cat ~/NCommerce/ncommerce_api/essentials/ngrok-files/example.yml >> ~/.ngrok2/ngrok.yml
+
+# Adicionar keep alive no tunnel do nlabs
+cat ~/NCommerce/ncommerce_api/essentials/ssh_config.txt >> ~/.ssh/config
+cd ~/.ssh && sudo chmod 600 ./config
 
 # Configurar nginx para ncommerce-api (back end)
 curl https://ncommerce.app:3001/setup/nginx_back/$2?token=$1 > /tmp/ncommerce-api
